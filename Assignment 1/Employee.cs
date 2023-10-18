62c1e397a8459aec47d9f929806f183c3ccca2ec
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 15:05:34 +0530

    Add project files.

---
+++ Assignment 1/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Employee
+    {
+        private int empNo;
+        private string empName;
+        private double salary;
+        private int deptNo;
+
+        public int EmpNo
+        {
+            get { return empNo; }
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentException("Employee number (empno) cannot be zero.");
+                empNo = value;
+            }
+        }
+
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Employee name (ename) cannot be null or empty.");
+                empName = value;
+            }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public int DeptNo
+        {
+            get { return deptNo; }
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentException("Department number (deptno) cannot be zero.");
+                deptNo = value;
+            }
+        }
+    }
+
+
+}
+
---
+++ Assignment 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                employee.EmpNo = 1234; 
+                employee.EmpName = "Rani"; 
+                employee.Salary = 10000.0;
+                employee.DeptNo = 0; 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
